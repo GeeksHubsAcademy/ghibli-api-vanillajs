caeb0ed0f4d26caa926e20aaa9cbab5d35fbca89
datata <dani@danitarazona.dev>
2023-10-02 12:55:10 +0200

    feat: ghibli api vanillajs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('hola')
+
+const films = document.getElementById('films')
+
+fetch('https://ghibliapi.vercel.app/films')
+.then(result => result.json())
+.then(result => {
+  result.forEach(element => {
+    const title = document.createElement('h1')
+    const filmImage = document.createElement('img')
+    
+    filmImage.src = element.image
+
+    filmImage.classList.add('image-film')
+
+    title.innerText = element.title
+    films.appendChild(title)
+    films.appendChild(filmImage)
+  });
+})
+.catch(error => console.log(error))
